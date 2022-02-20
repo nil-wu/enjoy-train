75cc4027ca35a7ed8232dab9c5da20c738b6b488
wuhaopeng <hp7855>
2022-02-20 19:45:53 +0800

    234

--- src/main/java/cn/enjoy/schd/ScheduledCase.java
+++ src/main/java/cn/enjoy/schd/ScheduledCase.java
@@ -8,8 +8,9 @@ public class ScheduledCase {
     public static void main(String[] args) {
 
         ScheduledThreadPoolExecutor schedule = new ScheduledThreadPoolExecutor(1);
-        schedule.scheduleAtFixedRate(new ScheduleWorker(ScheduleWorker.Normal), 1000, 3000, TimeUnit.SECONDS);
-        schedule.scheduleAtFixedRate(new ScheduleWorker(ScheduleWorker.HasException), 1000, 3000, TimeUnit.SECONDS);
+//        schedule.scheduleAtFixedRate(new ScheduleWorker(ScheduleWorker.Normal), 1000, 3000, TimeUnit.MILLISECONDS);
+        schedule.scheduleAtFixedRate(new ScheduleWorker(ScheduleWorker.HasException), 1000, 3000, TimeUnit.MILLISECONDS);
+//        schedule.scheduleAtFixedRate(new ScheduleWorker(ScheduleWorker.ProcessException), 1000, 3000, TimeUnit.MILLISECONDS);
 
 
     }
