aeb79cbef4e80dc331bf44590ac87eb791761b14
wuhaopeng <hp7855>
2022-05-28 21:49:55 +0800

    123

--- src/main/java/cn/enjoy/comps/CompletionCase.java
+++ src/main/java/cn/enjoy/comps/CompletionCase.java
@@ -1,16 +1,13 @@
 package cn.enjoy.comps;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CompletionCase {
     private final int POOL_SIZE = Runtime.getRuntime().availableProcessors();
     private final int TOTAL_TASK = Runtime.getRuntime().availableProcessors() * 10;
 
+    // 方法一，自己写集合来实现获取线程池中任务的返回结果
     public void testByQueue() throws Exception{
         long start = System.currentTimeMillis();
         AtomicInteger count = new AtomicInteger(0);
@@ -35,12 +32,40 @@ public class CompletionCase {
 
         pool.shutdown();
         System.out.println("-------------------- tasks sleep time " + count.get() + "ms,and spend time " + (System.currentTimeMillis() - start) + " ms");
+
+    }
+
+    // 方法二：通过 completionService 来实现获取线程池中任务的返回结果
+    public void testByCompletion()throws Exception{
+        long start = System.currentTimeMillis();
+        AtomicInteger count = new AtomicInteger(0);
+        //创建线程池
+        ExecutorService pool = Executors.newFixedThreadPool(POOL_SIZE);
+        CompletionService<Integer> cService = new ExecutorCompletionService<Integer>(pool);
         
+        //向里面扔任务
+        for (int i = 0; i < TOTAL_TASK; i++) {
+            cService.submit(new WorkTask("ExecTask" + i));
+        }
+
+        // 检查线程池任务执行结果
+        for (int i = 0; i < TOTAL_TASK; i++) {
+            Integer sleptTime = cService.take().get();
+            System.out.println(" slept " + sleptTime + " ms ...");
+            count.addAndGet(sleptTime);
+
+        }
+
+
+        //关闭线程池
+        pool.shutdown();
+        System.out.println("-------------------- tasks sleep time " + count.get() + "ms,and spend time " + (System.currentTimeMillis() - start) + " ms");
     }
 
     public static void main(String[] args) throws Exception {
         CompletionCase completionCase = new CompletionCase();
-        completionCase.testByQueue();
+//        completionCase.testByQueue();
+        completionCase.testByCompletion();
     }
 
 
